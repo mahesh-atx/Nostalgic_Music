0134020dec5df019bc040003a2ef99357e1c8d3e
Mahesh Dongare <129312491+mahesh-atx@users.noreply.github.com>
2026-08-11 22:41:01 +0530

    Fix React hydration error in Clock and finalize typography updates

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -16,19 +16,21 @@ function formatParts(date: Date) {
 }
 
 export default function Clock() {
+  const [mounted, setMounted] = useState(false);
   const [time, setTime] = useState(() => formatParts(new Date()));
 
   useEffect(() => {
+    setMounted(true);
     const id = setInterval(() => setTime(formatParts(new Date())), 1000);
     return () => clearInterval(id);
   }, []);
 
   return (
-    <div className="time">
-      {time.hour}
-      <span className="clock-colon">:</span>
-      {time.minute}
-      <span className="clock-period">{time.period}</span>
+    <div className="time" suppressHydrationWarning>
+      {mounted ? time.hour : ""}
+      <span className="clock-colon">{mounted ? ":" : ""}</span>
+      {mounted ? time.minute : ""}
+      <span className="clock-period">{mounted ? time.period : ""}</span>
     </div>
   );
 }
